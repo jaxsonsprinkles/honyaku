f342a600651f6a33c3e92b9c5473196dd7590099
terraboi4 <jaxstar24@gmail.com>
2026-02-25 11:58:46 -0600

    Beginning of client-server connections

--- backend/client_test.py
+++
@@ -1,9 +0,0 @@
-from websockets.sync.client import connect
-
-def hello():
-    with connect("ws://localhost:8765") as websocket:
-        message = websocket.recv()
-        print(message)
-
-if __name__ == "__main__":
-    hello()
--- frontend/src/renderer/App.tsx
+++ frontend/src/renderer/App.tsx
@@ -1,13 +1,14 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 function Main() {
   const ws = useRef<WebSocket>(null);
+  const [connected, setConnected] = useState<Boolean>(false);
 
   useEffect(() => {
-    const socket = new WebSocket('wss://echo.websocket.org/');
+    const socket = new WebSocket('wss://localhost:8765');
     socket.onopen = () => {
-      console.log('Connection established');
+      setConnected(true);
     };
 
     socket.onmessage = (event) => {
@@ -23,15 +24,32 @@ function Main() {
   };
 
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex items-center justify-center">
+    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 flex items-center justify-center text-gray-800">
       <div className="text-center space-y-6">
-        <h1 className="text-5xl font-bold text-gray-800">翻訳 (Honyaku)</h1>
-        <div className="flex gap-4 justify-center">
+        <h1 className="text-5xl font-bold">翻訳 (Honyaku)</h1>
+        <div className="flex flex-col gap-4 justify-center">
+          <div className="flex flex-col mx-auto gap-4 justify-center">
+            <p
+              className={`flex items-center gap-2 ${connected ? 'text-green-500' : 'text-red-500'}`}
+            >
+              <span className="relative flex h-3 w-3">
+                <span
+                  className={`animate-ping absolute inline-flex h-full w-full rounded-full ${connected ? 'bg-green-400' : 'bg-red-400'} opacity-75`}
+                ></span>
+                <span
+                  className={`relative inline-flex rounded-full h-3 w-3 ${connected ? 'bg-green-500' : 'bg-red-500'}`}
+                ></span>
+              </span>
+
+              {connected ? 'Connected' : 'Not Connected'}
+            </p>
+          </div>
+
           <button
             onClick={() => {
               sendMessage('Test');
             }}
-            className="border p-1 rounded-md"
+            className="border p-1 rounded-md cursor-pointer"
           >
             Send message
           </button>
